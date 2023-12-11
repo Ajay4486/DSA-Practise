143aa907f5f18ef748c987086112afbad45ae067
Ajay Shitkar <ajshitkar282@gmail.com>
2023-12-11 19:58:29 +0530

    BS

--- Binary Search/Peak Element in an array.cpp
+++ Binary Search/Peak Element in an array.cpp
@@ -9,26 +9,26 @@ Input: nums = [1,2,1,3,5,6,4]
 Output: 5
 Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.*/
 
-class Solution
-{
-    public:
-    int peakElement(int arr[], int n)
-    {
-       // Your code here
-        if(n==1) return 0;
-        if(arr[0]>arr[1]) return 0;
-        if(arr[n-1]>arr[n-2]) return n-1;
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
 
-        int low = 1 ; int high = n - 2;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
-               return mid;
-    
-            else if(arr[mid]>arr[mid-1])  low=mid+1;
-            
-            else if(arr[mid]>arr[mid+1])  high = mid-1;
+        if (n == 1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[n - 1] > nums[n - 2]) return n - 1;
+
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                return mid;
+            else if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1])
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
-    return -1;
+        return -1;
     }
 };
